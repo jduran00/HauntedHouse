ab8869d4733b182a308d721c9da035494bb8970a
jduran00 <157246481+jduran00@users.noreply.github.com>
2025-04-28 20:23:09 -0500

    Create HH_Env.py

---
+++ HH_Env.py
@@ -0,0 +1,28 @@
+import gymnasium as gym 
+import ale_py
+import numpy as np
+
+from collections import defaultdict
+
+from gymnasium.wrappers import GrayscaleObservation
+from gymnasium.wrappers import ResizeObservation
+from gymnasium.wrappers import FlattenObservation
+from gymnasium.wrappers import TransformObservation
+from gymnasium.wrappers import TimeLimit 
+
+gym.register_envs(ale_py)
+
+def process_env():
+    env = gym.make("ALE/HauntedHouse-v5", render_mode=None) #render None for speed, "human" to see how it works
+    env = GrayscaleObservation(env, keep_dim=False)
+    env = ResizeObservation(env, shape=(16, 16))  
+    env = TransformObservation(
+        env,
+        lambda obs: (obs // 32).astype(np.uint8),  
+        gym.spaces.Box(0, 7, shape=(8, 8), dtype=np.uint8)
+    )
+    env = FlattenObservation(env)  # make observation 1D for tuple key
+    
+    env = TimeLimit(env,max_episode_steps= 5000)
+    return env
+
